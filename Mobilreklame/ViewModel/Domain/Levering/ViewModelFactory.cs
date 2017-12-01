cf4edb80a603683ec359cc07e598d6cff27de6c3
Michael-MO <Michael-MO@users.noreply.github.com>
2017-12-01 11:18:08 +0100

    Added most classes

---
+++ Mobilreklame/ViewModel/Domain/Levering/ViewModelFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobilreklame.ViewModel.Domain.Levering
+{
+    class ViewModelFactory
+    {
+    }
+}
