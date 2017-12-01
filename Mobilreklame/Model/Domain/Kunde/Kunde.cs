3c0f258ca6b169310cf530d6caa86730c5b6cc4b
Michael-MO <Michael-MO@users.noreply.github.com>
2017-12-01 12:40:41 +0100

    Updated Class 'Kunde'

--- Mobilreklame/Model/Domain/Kunde/Kunde.cs
+++ Mobilreklame/Model/Domain/Kunde/Kunde.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataTransformation.Implementation;
 
 namespace Mobilreklame.Model.Domain.Kunde
 {
-    class Kunde
+    public class Kunde : TransformedBase<Kunde>
     {
+        public Kunde(
+            string name,
+            string phoneNumber,
+            string email,
+            string cvrNr,
+            string address,
+            int zipCode,
+            string company) : base(NullKey)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            CvrNr = cvrNr;
+            Address = address;
+            ZipCode = zipCode;
+            Company = company;
+        }
+
+        public Kunde() : base(NullKey)
+        {
+
+        }
+
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string CvrNr { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+        public string Company { get; set; }
+
+        public override void SetValuesFromObject(Kunde obj)
+        {
+            Key = obj.Key;
+            Name = obj.Name;
+            PhoneNumber = obj.PhoneNumber;
+            Email = obj.Email;
+            CvrNr = obj.CvrNr;
+            Address = obj.Address;
+            ZipCode = obj.ZipCode;
+            Company = obj.Company;
+        }
     }
 }
