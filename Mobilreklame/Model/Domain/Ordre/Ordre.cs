c96cedcea257989d4a3a3bc934693a3fb08114c3
Michael-MO <Michael-MO@users.noreply.github.com>
2017-12-06 11:03:20 +0100

    Created class 'Ordre'

--- Mobilreklame/Model/Domain/Ordre/Ordre.cs
+++ Mobilreklame/Model/Domain/Ordre/Ordre.cs
@@ -1,12 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DataTransformation.Implementation;
 
 namespace Mobilreklame.Model.Domain.Ordre
 {
-    class Ordre
+    public class Ordre : TransformedBase<Ordre>
     {
+        public Ordre(
+            int key,
+            int customerRefID,
+            DateTime date,
+            string description,
+            bool delivery,
+            bool extraProduction) : base(key)
+        {
+            CustomerRefID = customerRefID;
+            Date = date;
+            Description = description;
+            Delivery = delivery;
+            ExtraProduction = extraProduction;
+        }
+
+        public Ordre() : base(NullKey)
+        {
+
+        }
+
+        public int OrderID { get; set; }
+        public int CustomerRefID { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public bool Delivery { get; set; }
+        public bool ExtraProduction { get; set; }
+
+
+        public override void SetValuesFromObject(Ordre obj)
+        {
+            Key = obj.Key;
+            OrderID = obj.OrderID;
+            CustomerRefID = obj.CustomerRefID;
+            Date = obj.Date;
+            Description = obj.Description;
+            Delivery = obj.Delivery;
+            ExtraProduction = obj.ExtraProduction;
+        }
     }
 }
