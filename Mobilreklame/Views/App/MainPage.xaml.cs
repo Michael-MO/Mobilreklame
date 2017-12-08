3fc3deb53418a37f72f3ed98d609e793f6a05275
Michael-MO <Michael-MO@users.noreply.github.com>
2017-12-08 13:16:38 +0100

    Merge views from branch 'Daniel'

--- Mobilreklame/Views/App/MainPage.xaml.cs
+++ Mobilreklame/Views/App/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -12,6 +13,21 @@ namespace Mobilreklame.Views.App
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void ChangePageOrdre(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Domain.Ordre.View), null);
+        }
+
+        private void ChangeToMOnteringPage(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Domain.Levering.View), null);
+        }
+
+        private void ChangeToKundePage(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Domain.Kunde.View), null);
         }
     }
 }
--- Mobilreklame/Views/Domain/Ordre/View.xaml.cs
+++ Mobilreklame/Views/Domain/Ordre/View.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +13,16 @@ namespace Mobilreklame.Views.Domain.Ordre
         public View()
         {
             this.InitializeComponent();
+        }
+
+        private void ChangeToMonteringPage(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Levering.View), null);
+        }
+
+        private void ChangeToKundePage(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Kunde.View), null);
         }
     }
 }
